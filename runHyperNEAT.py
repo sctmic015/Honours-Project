23782e59ad15754d4388ced1384168fd4c99e32d
sctmic015 <michaelscott.scott77@gmail.com>
2022-08-01 14:35:24 +0200

    Stuff for the cloud

--- runHyperNEAT.py
+++ runHyperNEAT.py
@@ -116,7 +116,7 @@ def run(gens):
 
 
 if __name__ == '__main__':
-    WINNER = run(5)[0]  # Only relevant to look at the winner.
+    WINNER = run(1000)[0]  # Only relevant to look at the winner.
     print("This is the winner!!!")
     print(type(WINNER))
     print('\nBest genome:\n{!s}'.format(WINNER))
